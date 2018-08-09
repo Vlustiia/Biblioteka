62b679946626fcb909c3deb9ea1c7cbd467666d1
Evgeniy Ovchinnikov <khatangatao@gmail.com>
2018-08-09 19:40:23 +0700

    Создана домашняя страница. Провел первые эксперименты с шаблонами

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Доступные книги (статус = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_special_books = Book.objects.filter(title__contains='колоб').count()
+
+    # метод all() применен по-умолчанию
+    num_authors = Author.objects.count()
+    num_genres = Genre.objects.count()
+
+    #отрисовка HTML шаблона
+    return render(
+        request,
+        'index.html',
+        context={'num_books':num_books,
+                 'num_instances':num_instances,
+                 'num_instances_available':num_instances_available,
+                 'num_author':num_authors,
+                 'num_special_books':num_special_books,
+                 'num_genres':num_genres
+                 },
+    )
