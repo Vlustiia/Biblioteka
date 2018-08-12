2e4b506622e72439bfb15d75f61a9cd86f585003
Evgeniy Ovchinnikov <khatangatao@gmail.com>
2018-08-12 18:49:21 +0700

    Сделаны страницы для списков книг, авторов. Сделаны подробные страницы для авторов

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -28,5 +28,21 @@ def index(request):
                  },
     )
 
+
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 4
+
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 4
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
