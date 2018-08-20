b5bc10e4abb1f858344521c62d30e0141df21692
Evgeniy Ovchinnikov <khatangatao@gmail.com>
2018-08-20 09:54:11 +0700

    Завершена работа над формами сброса пароля

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = '/static/'
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
+
+# регистрация писем, отправленных на консоль
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
